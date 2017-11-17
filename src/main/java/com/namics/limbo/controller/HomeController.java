c98f3eaab1cc436374770f9fdbabdeb985f50023
Lorenz Bösch <lorenz.boesch@namics.com>
2017-11-17 13:31:35 +0100

    test once again.

--- src/main/java/com/namics/limbo/controller/HomeController.java
+++ src/main/java/com/namics/limbo/controller/HomeController.java
@@ -1,11 +1,7 @@
 package com.namics.limbo.controller;
 
-import com.frogermcs.gactions.ResponseBuilder;
-import com.frogermcs.gactions.api.request.RootRequest;
-import com.frogermcs.gactions.api.response.RootResponse;
 import com.namics.limbo.model.GActionRequest;
 import com.namics.limbo.model.GActionResponse;
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -15,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 
 /**
@@ -42,21 +40,45 @@ public class HomeController {
 		}
 
 		if (number == null) {
-			return new GActionResponse().speech("wo ist denn die zahl?");
+			return new GActionResponse().speech("wo ist denn die zahl?").displayText("****obacht****");
+		}
+		if (number == 1) {
+			return new GActionResponse().displayText("Achtung, lustig.")
+			                            .speech("Beat hat noch nicht genug Witze gehört. Was findet man beim Kannibalen in der Dusche? — Head and Shoulders.");
+		}
+		if (number == 2) {
+			return new GActionResponse().displayText("Achtung, lustig.")
+			                            .speech("Beat hat noch nicht genug Witze gehört. Wie heißt ein Spanier ohne Auto? — Carlos");
+		}
+		if (number == 3) {
+			return new GActionResponse().displayText("Achtung, lustig.")
+			                            .speech("Was ist ein studierter Bauer? —- Ein Ackerdemiker");
+		}
+		if (number == 4) {
+			return new GActionResponse().displayText("Achtung, lustig.")
+			                            .speech("Hopp Sangallä, fürä mit em Ballä.");
 		}
 		return new GActionResponse().speech("du hast limbo gesagt. wer kommt schon auf limbo " + number);
 	}
 
-	@PostMapping(value = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
-	public GActionResponse test(InputStream inputStream) throws IOException {
-		log.error("POST to test: {}", IOUtils.toString(inputStream));
-		RootResponse rootResponse = ResponseBuilder.tellResponse("You've just said: ueli");
-		log.error("GActionResponse: {}", rootResponse);
-		return new GActionResponse().speech("Hast du den ueli gemeint gäll?");
+	@PostMapping(value = "/bonus", produces = MediaType.APPLICATION_JSON_VALUE)
+	public GActionResponse test(@RequestBody GActionRequest rootRequest) throws IOException {
+		log.error("POST: " + rootRequest);
+
+		if (rootRequest.getResult().getParameters().containsKey("name")) {
+			int n = new Random().nextInt(20000) + 1;
+			String prefix = n > 1000 ? "good job " : "uiuiui schlechtes jahr: ";
+			String name = rootRequest.getResult().getParameters().get("name");
+			if (name.equalsIgnoreCase("beat") || name.equalsIgnoreCase("helfenberger")) {
+				return new GActionResponse().speech(name + "erhält dieses Jahr kein Bonus, er möchte nur Witze hören");
+			}
+			return new GActionResponse().speech(prefix + name + "erhält dieses Jahr einen Bonus von " + n);
+		}
+		List<String> names = Arrays.asList("Fredy", "Ueli", "Gorgio Contini", "Killo Barnetta", "Kyponto");
+		return new GActionResponse().speech("du hast keinen Mitarbeiter angegeben, um den Bonus zu ermittlen. Der Mitarbeiter des Jahres ist aber " + names
+				.stream()
+				.findAny()
+				.orElse("Theo"));
 	}
 
-	private RootRequest parseActionRequest(RootRequest request) throws IOException {
-		log.info("REQUEST:" + request);
-		return request;
-	}
 }
